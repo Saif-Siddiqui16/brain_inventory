28c35702d2f323a57dd029169e9c95a022540d06
Mohd. Saif Siddiqui <siddiqui16saif@gmail.com>
2025-05-29 01:55:01 +0530

    backend: Changed index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import { connectDB } from "./db/connectDb.js"
+import authRoutes from "./routes/auth.route.js"
+import groupRoutes from "./routes/group.route.js"
+import expenseRoutes from "./routes/expense.route.js"
 
 dotenv.config()
 
@@ -11,7 +14,17 @@ const PORT=process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors(
+{
+        origin: "http://localhost:5173",
+    credentials: true,
+}
+))
+
+//routes
+app.use("/api/auth",authRoutes)
+app.use("/api/group",groupRoutes)
+app.use("/api/expense",expenseRoutes)
 
 app.listen(PORT,async()=>{
     await connectDB()
